724fbe2fb2406521d5f9818b6f9a4b0edb789046
Apoorv Singh <singhapo@msu.edu>
2015-02-16 11:06:02 -0500

    refactored erb project

--- app.rb
+++ app.rb
@@ -2,8 +2,8 @@
 require 'sinatra'
 
 class MyWebApp < Sinatra::Base
-	get '/' do
-		last_modified Time.now
-		send_file "views/index#{ rand(3) + 1}.html"
-	end
+  get '/' do
+    last_modified Time.now
+    erb :"index#{ rand(3) + 1}"
+  end
 end
